29e86e25fe72efa53e1c1e11dd5a695bd1801379
mwvane <bzishvili57@gmail.com>
2022-12-21 19:05:18 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-shop';
+  youtubeIcon = faYoutube
 }
