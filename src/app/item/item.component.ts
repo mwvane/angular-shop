30c19e4afe20bee3dc4962df36df8aee663c9747
mwvane <bzishvili57@gmail.com>
2022-12-22 19:05:35 +0400

    Use quantity-selector in item component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { faCartPlus} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent {
-  
+  cart = faCartPlus
+  quantity: number = 1
+  quantityUpdate(quantity: number){
+    this.quantity = quantity
+  }
 }
